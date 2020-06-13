00a8f1cdccf696c8eb3f89a18c5b9f480d092f97
Marcos Rogerio <marcos.pinheiro@fbter.org.br>
2020-06-13 20:21:15 -0300

    service ok

--- Front/src/app/modules/core/generics/api.ts
+++
@@ -1,1 +0,0 @@
-export const SALE_API = 'http://localhost:8080'
--- Front/src/app/modules/core/models/cliente.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Cliente{
-  id: number;
-  nome: string
-	cpf: number;
-	rg: number;
-	descricao: string;
-	email: string
-}
